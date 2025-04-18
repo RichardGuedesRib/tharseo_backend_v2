5254a9a1e89b1928d816cd78f1677ea7aaf2303f
Richard Guedes <richard.silva46@fatec.sp.gov.br>
2025-04-18 19:02:27 -0300

    test : implementado testes unitários na camada controller de health

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('HealthController', () => {
   let controller: HealthController;
@@ -12,7 +13,26 @@ describe('HealthController', () => {
     controller = module.get<HealthController>(HealthController);
   });
 
-  it('should be defined', () => {
+  it('deve ser instanciado', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('check', () => {
+    it('deve retornar que o servidor está operacional', () => {
+      const result = controller.check();
+      expect(result).toEqual('Application is up and running');
+    });
+
+    it('deve retornar um InternalServerErrorException indicando que não está operacional', () => {
+      jest.spyOn(controller, 'check').mockImplementation(() => {
+        throw new InternalServerErrorException();
+      });
+  
+      try {
+        controller.check();
+      } catch (error) {
+        expect(error).toBeInstanceOf(InternalServerErrorException);
+      }
+    });
   });
 });
